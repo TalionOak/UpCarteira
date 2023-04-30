659d701756229d9b31207f67e12510ff1162f971
TalionOak <ailtonsscarvalho@gmail..com>
2023-04-29 22:39:31 -0300

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Sua_Carteira {
+  partial class Form1 {
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      this.panelTop = new System.Windows.Forms.Panel();
+      this.panelLeft = new System.Windows.Forms.Panel();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.panelTop.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // panelTop
+      // 
+      this.panelTop.Controls.Add(this.pictureBox1);
+      this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+      this.panelTop.Location = new System.Drawing.Point(0, 0);
+      this.panelTop.Name = "panelTop";
+      this.panelTop.Size = new System.Drawing.Size(1213, 115);
+      this.panelTop.TabIndex = 0;
+      // 
+      // panelLeft
+      // 
+      this.panelLeft.Dock = System.Windows.Forms.DockStyle.Left;
+      this.panelLeft.Location = new System.Drawing.Point(0, 115);
+      this.panelLeft.Name = "panelLeft";
+      this.panelLeft.Size = new System.Drawing.Size(168, 519);
+      this.panelLeft.TabIndex = 1;
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
+      this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(168, 115);
+      this.pictureBox1.TabIndex = 2;
+      this.pictureBox1.TabStop = false;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1213, 634);
+      this.Controls.Add(this.panelLeft);
+      this.Controls.Add(this.panelTop);
+      this.Name = "Form1";
+      this.Text = "Sua Carteira";
+      this.panelTop.ResumeLayout(false);
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private Panel panelTop;
+    private PictureBox pictureBox1;
+    private Panel panelLeft;
+  }
+}
---
+++ Form1.cs
@@ -0,0 +1,7 @@
+namespace Sua_Carteira {
+  public partial class Form1 : Form {
+    public Form1() {
+      InitializeComponent();
+    }
+  }
+}
